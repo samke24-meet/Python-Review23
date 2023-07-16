d5a413fd061469cb004230b7022edca56d5e04f8
samke24-meet <bokutosam0@gmail.com>
2023-07-16 17:12:15 +0300

    Create Review.py

---
+++ Review.py
@@ -0,0 +1,27 @@
+def create_youtube_video(title,description) :
+	Video = { "title" : title , "description" : description , "likes" : 0 , "comments" : comments = {} }
+	return video
+
+def like(video) :
+	if "likes" in video :
+		video["likes"] = video["likes"] + 1 
+	return video
+
+def dislike(video) :
+	if "dislikes" in video :
+		video["dislikes"] = video["dislikes"] + 1 
+	return video
+
+def add_comment( youtubevideo , username , comment_text ) :
+	youtubevideo["comments"[username] = comment_text]
+	return youtubevideo
+
+n = create_youtube_video("DINOSAUR", "DINOSAUR EATING CAKE")
+print(n)
+like(n)
+n["dislikes"] = 0
+dislike(n)
+add_comment( n , "hadeel" , "I like tiramisu")
+print(n)
+n["likes"] = n["likes"]+495
+print(n["likes"])
